adcb6f02e00ef68196a5c5913ac24c1851218ded
tujestem <marcin.skrobek@windowslive.com>
2020-06-28 20:31:51 +0200

    commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 // kometarz z g.2022
+// komentarz z g.2031
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
